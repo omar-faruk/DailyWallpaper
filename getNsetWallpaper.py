f4f9fd6cc2c8c6160aa218cbc0218472bd8dc648
Omar Faruk <munna93_bd@yahoo.com>
2017-11-21 22:22:02 +0600

    wallpaper change support for unity/gnome environment added, automatic directory creation added

--- getNsetWallpaper.py
+++ getNsetWallpaper.py
@@ -5,53 +5,50 @@ import os
 import getpass
 
 
-
 def getURL():
-	xml_data=""
+	xml_data = ""
 	url = "http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-US"
 	###change idx to 1,2,3,4... to next wallpapers
 
-	bing_url="http://www.bing.com/"
-	img_url=""
+	bing_url = "http://www.bing.com/"
+	img_url = ""
 	with contextlib.closing(urllib.request.urlopen(url)) as fp:
-		while(True):
-			block=fp.read(1024*8)
+		while (True):
+			block = fp.read(1024 * 8)
 			if not block:
 				break
-			xml_data+=(str(block))
+			xml_data += (str(block))
 
-	img_location=re.findall("<url>*.*</url>",xml_data)
-	img=img_location[0]
-	img_url=str(img.replace("<url>",''))
-	img_url=str(img_url.replace("</url>",''))
-	img_url=str(bing_url+img_url)
-	#print(img_url)
+	img_location = re.findall("<url>*.*</url>", xml_data)
+	img = img_location[0]
+	img_url = str(img.replace("<url>", ''))
+	img_url = str(img_url.replace("</url>", ''))
+	img_url = str(bing_url + img_url)
+	# print(img_url)
 	return img_url
 
 
-
 def getImage(image_url):
+	url_parse = img_url.split('/')
+	image_file_name = url_parse[len(url_parse) - 1]
+	# print(image_file_name)
 
-	url_parse=img_url.split('/')
-	image_file_name=url_parse[len(url_parse)-1]
-	#print(image_file_name)
+	system_user = getpass.getuser()
+	file_location = "/home/" + str(system_user) + "/Pictures/BingDaily/"
 
-	system_user=getpass.getuser()
-	file_location="/home/"+str(system_user)+"/Pictures/BingDaily/"
+	image_file_name = file_location + image_file_name
 
-	image_file_name=file_location+image_file_name
-
-	#if the current bing wallpaper exists we dont need to download it
+	# if the current bing wallpaper exists we dont need to download it
 	if os.path.exists(image_file_name):
 		print("file exits we dont need to download")
 		return image_file_name
 
-	#if doesnt exists we need to download it
+	# if doesnt exists we need to download it
 	else:
-		file=open(image_file_name,'wb')
+		file = open(image_file_name, 'wb')
 		with contextlib.closing(urllib.request.urlopen(image_url)) as rfp:
-			while(True):
-				block=rfp.read(1024*8)
+			while (True):
+				block = rfp.read(1024 * 8)
 				if not block:
 					break
 				file.write(block)
@@ -59,17 +56,25 @@ def getImage(image_url):
 		return image_file_name
 
 
-
 def setWallapper(file_name):
-	command= "gsettings set org."+os.getenv('DESKTOP_SESSION')+".desktop.background picture-uri "
-	file_name=file_name
-	final_command=command+"file://"+file_name
+	desktop_environment = os.getenv('DESKTOP_SESSION')
+
+	if (desktop_environment == "ubuntu"):
+		desktop_environment = "gnome"
+	elif (desktop_environment == "gnome2"):
+		desktop_environment = "gnome"
+	elif (desktop_environment == "gnome3"):
+		desktop_environment = "gnome"
+
+	command = "gsettings set org." + desktop_environment + ".desktop.background picture-uri "
+	file_name = file_name
+	final_command = command + "file://" + file_name
 	print(final_command)
 	os.system(final_command)
 	return None
 
 
-if __name__=="__main__":
-	img_url=getURL()
-	image_file_name=getImage(img_url)
+if __name__ == "__main__":
+	img_url = getURL()
+	image_file_name = getImage(img_url)
 	setWallapper(image_file_name)
